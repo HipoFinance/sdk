e80477a92f6e4eab4c3c4ef1ee38b3d2980c5d36
Behrang Norouzinia <behrangn@gmail.com>
2024-05-11 08:54:35 +0330

    Replace Record with Map

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,9 +1,9 @@
 import { Address } from '@ton/ton'
 
-export const treasuryAddresses: Record<string, Address> = {
-    mainnet: Address.parse('EQCLyZHP4Xe8fpchQz76O-_RmUhaVc_9BAoGyJrwJrcbz2eZ'),
-    testnet: Address.parse('kQAlDMBKCT8WJ4nwdwNRp0lvKMP4vUnHYspFPhEnyR36cg44'),
-}
+export const treasuryAddresses = new Map<string, Address>([
+    ['mainnet', Address.parse('EQCLyZHP4Xe8fpchQz76O-_RmUhaVc_9BAoGyJrwJrcbz2eZ')],
+    ['testnet', Address.parse('kQAlDMBKCT8WJ4nwdwNRp0lvKMP4vUnHYspFPhEnyR36cg44')],
+])
 
 export const opDepositCoins = 0x3d3761a6
 export const opUnstakeTokens = 0x595f07bc
