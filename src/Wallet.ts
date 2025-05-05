c2677225bcc6c1ecd3a35c72bbfe94e28352b7a1
Behrang Norouzinia <behrangn@gmail.com>
2025-05-05 19:14:01 +0330

    Add sendUnstakeTokens wrapper function

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,4 +1,11 @@
-import { Address, Contract, ContractProvider, Dictionary } from '@ton/ton'
+import { Address, beginCell, Cell, Contract, ContractProvider, Dictionary, Sender, SendMode } from '@ton/ton'
+import { opUnstakeTokens } from './Constants'
+
+export enum UnstakeMode {
+    Auto, // 0
+    Instant, // 1
+    Best, // 2
+}
 
 export interface WalletState {
     tokens: bigint
@@ -24,5 +31,40 @@ export class Wallet implements Contract {
             ),
             unstaking: stack.readBigNumber(),
         }
+    }
+
+    async sendUnstakeTokens(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint | string
+            bounce?: boolean
+            sendMode?: SendMode
+            queryId?: bigint
+            tokens: bigint
+            returnExcess?: Address
+            mode?: UnstakeMode
+            ownershipAssignedAmount?: bigint
+        },
+    ) {
+        let customPayload: Cell | null = null
+        if (opts.ownershipAssignedAmount != null || opts.mode != null) {
+            customPayload = beginCell()
+                .storeUint(opts.mode ?? 0, 4)
+                .storeCoins(opts.ownershipAssignedAmount ?? 0)
+                .endCell()
+        }
+        await provider.internal(via, {
+            value: opts.value,
+            bounce: opts.bounce,
+            sendMode: opts.sendMode,
+            body: beginCell()
+                .storeUint(opUnstakeTokens, 32)
+                .storeUint(opts.queryId ?? 0, 64)
+                .storeCoins(opts.tokens)
+                .storeAddress(opts.returnExcess)
+                .storeMaybeRef(customPayload)
+                .endCell(),
+        })
     }
 }
